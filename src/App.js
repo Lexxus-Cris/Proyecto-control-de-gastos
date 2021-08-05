2e807cba49c71b8fcce061c43ea88edaf91e45ac
Uriel Cristino <urielcris.97@gmail.com>
2021-08-04 19:12:07 -0500

    Estructuracion de carpetas y configuracion de archivos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import Pregunta from "./components/Pregunta";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="container">
+      <header>
+        <h1>Control de gastos semanal</h1>
+        <div className="contenido-principal contenido">
+          <Pregunta />
+        </div>
+      </header>
     </div>
   );
 }
